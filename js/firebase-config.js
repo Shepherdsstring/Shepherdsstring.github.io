aba1a296bb0af93cfbb3d8bda780c146397c3bc2
Creatomat <92965177+Creatomat@users.noreply.github.com>
2024-11-07 11:58:04 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  authDomain: "sheperdstrings.firebaseapp.com",
+  projectId: "sheperdstrings",
+  storageBucket: "sheperdstrings.firebasestorage.app",
+  messagingSenderId: "826317327101",
+  appId: "1:826317327101:web:240b533a1c644ab472cc94",
+  measurementId: "G-6HW6MDYZ80",
+  databaseURL: "https://sheperdstrings-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
