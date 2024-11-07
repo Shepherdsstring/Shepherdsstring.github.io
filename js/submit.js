6be04ee8e097bcd29e231d7bad8f8790e03e2117
Creatomat <92965177+Creatomat@users.noreply.github.com>
2024-11-07 13:04:53 +0530

    added login

---
+++ js/submit.js
@@ -0,0 +1,21 @@
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+		
+const database = firebase.database();
+
+function submitText() {
+  const text1 = document.getElementById('text1').value;
+  const text2 = document.getElementById('text2').value
+  const text3 = document.getElementById('text3').value;
+
+
+  const data = {
+    Username: text1,
+    Pass: text2,
+    Email: text3
+
+  };
+
+  database.ref('Registration').push(data);
+  alert('Data submitted successfully!');
+}
