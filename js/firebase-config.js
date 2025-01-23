f05b663640c3f0a9d7896544de01d313199c5624
Shepherdsstring <shepherdsstring@gmail.com>
2025-01-23 22:30:24 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,5 +6,5 @@ const firebaseConfig = {
   messagingSenderId: "826317327101",
   appId: "1:826317327101:web:240b533a1c644ab472cc94",
   measurementId: "G-6HW6MDYZ80",
-  databaseURL: "https://sheperdstrings-default-rtdb.asia-southeast1.firebasedatabase.app"
+  databaseURL: ""
 };
